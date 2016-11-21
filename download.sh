1c201bfad87952332338068aab24abcd05c5a7c3
Marcin Nowicki <pr0d1r2@gmail.com>
2016-11-21 07:24:41 +0100

    Download assets for local usage

---
+++ download.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+which axel 2>/dev/null 1>/dev/null
+if [ $? -eq 0 ]; then
+  CMD="axel"
+else
+  CMD="wget"
+fi
+
+if [ ! -f portage-20161119.tar.xz ]; then
+  $CMD http://mirror.switch.ch/ftp/mirror/gentoo/releases/snapshots/current/portage-20161119.tar.xz || return $?
+fi
+
+if [ ! -f stage3-amd64-systemd-20161113.tar.bz2 ]; then
+  $CMD http://mirror.switch.ch/ftp/mirror/gentoo/releases/amd64/autobuilds/current-stage3-amd64-systemd/stage3-amd64-systemd-20161113.tar.bz2
+fi
+
+if [ ! -f stage4-amd64-cloud-20161117.tar.bz2 ]; then
+  $CMD http://mirror.switch.ch/ftp/mirror/gentoo/releases/amd64/autobuilds/current-stage4-amd64-cloud/stage4-amd64-cloud-20161117.tar.bz2
+fi
+
+if [ ! -f stage4-amd64-hardened+cloud-20161117.tar.bz2 ]; then
+  $CMD http://mirror.switch.ch/ftp/mirror/gentoo/releases/amd64/autobuilds/current-stage4-amd64-hardened+cloud/stage4-amd64-hardened+cloud-20161117.tar.bz2
+fi
